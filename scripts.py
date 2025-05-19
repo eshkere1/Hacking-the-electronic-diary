4811f0a012b6e27c047753c4a98d61247c2f5b7e
Pavel <piv17032009@gmail.com>
2025-05-19 17:56:18 +0300

    fix

--- scripts.py
+++ scripts.py
@@ -8,19 +8,24 @@ load_dotenv()
 
 def get_schoolkid():
     try:
-        schoolkid = Schoolkid.objects.get(full_name=os.getenv(KID_NAME, "Фролов Иван Григорьевич"))
+        schoolkid = Schoolkid.objects.get(full_name=os.getenv("KID_NAME", "Фролов Иван Григорьевич"))
+        return schoolkid
     except ObjectDoesNotExist:
-        print('Does Not Exist!')
-    return schoolkid
+        raise ValueError("Ученик не найден")
+    except MultipleObjectsReturned:
+        raise ValueError("Найдено несколько учеников")
+    
 
-def fix_marks():
+def fix_marks(schoolkid):
     Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
 
-def remove_chastisements():
+
+def remove_chastisements(schoolkid):
     chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
     chastisements.delete()
     
-def create_commendation(subject_title):
+    
+def create_commendation(schoolkid, subject_title):
     lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject__title=subject_title).first()
     if lesson:
         Commendation.objects.create(text="Хвалю!", created=lesson.date, schoolkid=schoolkid, subject=lesson.subject, teacher=lesson.teacher)
